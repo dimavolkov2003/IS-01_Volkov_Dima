741173b4b58759766b3abbb222ad3973d11571a9
Dima_Volkov <dima.volkov1704@gmail.com>
2020-12-23 01:40:02 +0200

    change bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 counter=1
 for file in `ls *.*`; do
-	mv $file $counter$file
-	counter=$(($counter+1))
+	if [ $0 != $file ]; then
+		mv $file $counter$file
+		counter=$(($counter+1))
+	fi
 done
+
